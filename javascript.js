03bc4cb84a013edc44e23a5f39fc27f1ff56231b
Mark Loeser <mark.loeser@gmail.com>
2016-09-12 11:08:59 -0400

    switch units and clean icon
    added ability to switch temp units and cleaned up the icon lookup - now a switch/case instead of huge if else statement.

--- javascript.js
+++ javascript.js
@@ -1,62 +1,103 @@
 // Forecast.io url with key https://api.forecast.io/forecast/8188d2efca90a6f6a9a9b53f07280a0f/37.8267,-122.423
 
-//creating global variables for lattitude and longitude - will later change these in geolocation function but I think I have to set them outside so they are global
-var lat =0;
-var lon=0;
 
-if (navigator.geolocation) {
- navigator.geolocation.getCurrentPosition(function(position) {
+   navigator.geolocation.getCurrentPosition(function(position) {
     // sets lat and lon variables to be used in api call 
-    lat = position.coords.latitude;
-    lon = position.coords.longitude
-  });
-}
+   var lat = position.coords.latitude;
+   var lon = position.coords.longitude;
+   
+   function getWeather(){
 
-//function to send forecast.io api call and retrieve weather data and also include if/else statement to pull the weather icon that matches the conditions
-function getWeather(){
+   var apiKey = '8188d2efca90a6f6a9a9b53f07280a0f';
+   var url = 'https://api.forecast.io/forecast/';
+   var data;
+   var units = "°F"
+            
 
-            var apiKey = '8188d2efca90a6f6a9a9b53f07280a0f';
-            var url = 'https://api.forecast.io/forecast/';
-            var data;
+   $.getJSON(url + apiKey + "/" + lat + "," + lon + "?callback=?", function(data) {
+            
+   var temp =  data.currently.temperature; 
+              
+   $('#weather').html(Math.round(temp) + units);
+              
+                                          $('#conditions').html(data.currently.summary);
+            //can uncomment and add element with id=position to html to display position to make sure it's working   
+            // $('#position').html(lat + " " + lon);
+              
+         // switch case to pick weather icon based off the data
+            switch (data.currently.icon){
+              case "partly-cloud-day":
+                $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473420467/partycloudy_wdnbaf.png');
+                break;
+              case "clear-day":
+                $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473420650/Sunny_bb9hea.png');  
+                break;
+              case "clear-night":
+                $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473420823/clearnight_rdofup.png');
+                break;
+              case "rain":
+                $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473421027/rain_dvzcrm.png');
+                break;
+              case "sleet":
+                $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473421294/sleet_yo5wms.png');
+                break;
+              case "fog":
+                $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473421411/fog_y9tcew.png');
+                break;
+              case "partly-cloudy-night":
+                $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473421302/partlycloudynight_h3exfn.png');
+                break;
+              case "snow":
+                $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473421294/snow_e8jd8a.png');
+                break;
+              case "cloudy":
+                $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473421860/cloudy_wwcrdq.png');
+                break;
+              case "wind":
+                $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473421294/wind_stnlkv.png');
+                break;
+              default:
+                $("#icon").attr('src', "http://ih0.redbubble.net/image.92064828.0090/sticker,375x360.u3.png");      
+          }
 
-            $.getJSON(url + apiKey + "/" + lat + "," + lon + "?callback=?", function(data) {
-              //console.log(data);
-            var temp =  data.currently.temperature; $('#weather').html(Math.round(temp) + "°F");
-              $('#conditions').html(data.currently.summary);
-              //if statement to select right icon - could not figure out how to do this with a javascript object because icon data from forecast.io has dashes in it
-              if (data.currently.icon == "partly-cloudy-day"){
-              $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473420467/partycloudy_wdnbaf.png')      
-              } else if (data.currently.icon == "clear-day"){
-              $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473420650/Sunny_bb9hea.png') 
-              } else if (data.currently.icon == "clear-night"){
-              $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473420823/clearnight_rdofup.png') 
-              } else if (data.currently.icon == "rain"){
-              $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473421027/rain_dvzcrm.png') 
-              } else if (data.currently.icon == "sleet"){
-              $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473421294/sleet_yo5wms.png') 
-              } else if (data.currently.icon == "fog"){
-              $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473421411/fog_y9tcew.png') 
-              } else if (data.currently.icon == "partly-cloudy-night"){
-              $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473421302/partlycloudynight_h3exfn.png') 
-              } else if (data.currently.icon == "snow"){
-              $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473421294/snow_e8jd8a.png') 
-              } else if (data.currently.icon == "cloudy"){
-              $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473421860/cloudy_wwcrdq.png') 
-              } else if (data.currently.icon == "wind"){
-              $("#icon").attr('src', 'http://res.cloudinary.com/dpyptkefp/image/upload/v1473421294/wind_stnlkv.png') 
-              }
+          $("#weather").click(function(){
+            if (units == "°F") {
+              units = "°C";
+              $('#weather').html(Math.round((temp-32)*5/9) + units);
+            } else {
+              units = "°F";
+              $('#weather').html(Math.round(temp) + units);
+            }
+      
+    });
+       
+      
+              
               
             });
-        }
+        } 
 
 
 //runs the getWeather function on page load
 $(document).ready(function(){
   
   getWeather();
-  $('#check').html(temp);
 });
+     
+
+   
+   
+   
+   
+   
+   
+   
+   
+  });
+
+
+//function to send forecast.io api call and retrieve weather data and also include if/else statement to pull the weather icon that matches the conditions
+
+
+
 //click temperature to switch units between C and F
-$('#weather').click(function(){
-  $('#weather').html(Math.round((((data.currently.temperature)-32) * 5/9)) + "°C");
-})
